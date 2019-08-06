61a313c93b2775f52ff62509cba3279852242393
Prashant Khanduri <prashant.khanduri@gmail.com>
2019-08-07 02:23:17 +0530

    production push wrapup

--- gatsby/src/components/reuse/social.jsx
+++ gatsby/src/components/reuse/social.jsx
@@ -6,10 +6,10 @@ export default function SocialComponent(props){
 
   return (
     <div className="">
-      {contact.github ? <a href={"https://github.com/"+contact.github} className="m-1 btn btn-sm btn-dark"><i className="fab fa-github fa-2x"></i></a>: null }
-      {contact.linkedin ? <a href={"https://www.linkedin.com/in/"+contact.linkedin} className="m-1 btn btn-sm btn-dark"><i className="fab fa-linkedin fa-2x"></i></a>: null }
-      {contact.facebook ? <a href={"https://www.facebook.com/"+contact.facebook} className="m-1 btn btn-sm btn-dark"><i className="fab fa-facebook fa-2x"></i></a>: null }
-      {contact.twitter ? <a href={"https://twitter.com/"+contact.twitter} className="m-1 btn btn-sm btn-dark"><i className="fab fa-twitter fa-2x"></i></a>: null }
+      {contact.github ? <a aria-label="github" href={"https://github.com/"+contact.github} className="m-1 btn btn-sm btn-dark"><i className="fab fa-github fa-2x"></i></a>: null }
+      {contact.linkedin ? <a aria-label="linkedin" href={"https://www.linkedin.com/in/"+contact.linkedin} className="m-1 btn btn-sm btn-dark"><i className="fab fa-linkedin fa-2x"></i></a>: null }
+      {contact.facebook ? <a aria-label="facebook" href={"https://www.facebook.com/"+contact.facebook} className="m-1 btn btn-sm btn-dark"><i className="fab fa-facebook fa-2x"></i></a>: null }
+      {contact.twitter ? <a aria-label="twitter" href={"https://twitter.com/"+contact.twitter} className="m-1 btn btn-sm btn-dark"><i className="fab fa-twitter fa-2x"></i></a>: null }
     </div>
   )
 }
